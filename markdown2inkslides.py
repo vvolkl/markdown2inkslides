91e9189fa70b83c6f6de122338d18d8ceb8b5376
Valentin Volkl <valentin.volkl@cern.ch>
2016-06-09 15:42:48 +0200

    Add id checks to image and text handlers.

--- markdown2inkslides.py
+++ markdown2inkslides.py
@@ -22,16 +22,16 @@ def debug_print(msg):
 
 def handle_header(self, line):
     titletext = line
-    already_existing_layer = self.svg.find('.//{http://www.w3.org/2000/svg}g[@id="layer%i"]' % (self.layercounter))
+    already_existing_layer = self.svg.find('.//{http://www.w3.org/2000/svg}g[@id="md2i_layer%i"]' % (self.layercounter))
     if already_existing_layer is None:
       self.layers.append(inkex.etree.SubElement(self.svg, 'g'))
       self.layer = self.layers[-1]
       self.layer.set(inkex.addNS('label', 'inkscape'), 'layer%s' % (self.layercounter))
       self.layer.set(inkex.addNS('groupmode', 'inkscape'), 'layer')
-      self.layer.set('id', 'layer%i' % (self.layercounter) )
+      self.layer.set('id', 'md2i_layer%i' % (self.layercounter) )
       # write slide number
       text = inkex.etree.Element(inkex.addNS('text','svg'))
-      text.set('id', 'layer%i_slidenumber' % (self.layercounter) )
+      text.set('id', 'md2i_layer%i_slidenumber' % (self.layercounter) )
       text.text = str(self.layercounter)
       # lower right corner
       text.set('x', str(11 * self.width / 12.))
@@ -51,32 +51,45 @@ def handle_header(self, line):
       # update title
       self.layers.append(already_existing_layer)
       self.layer = self.layers[-1]
-      title = already_existing_layer.find('.//{http://www.w3.org/2000/svg}text[@id="layer%i_title"]' % (self.layercounter))
+      title = already_existing_layer.find('.//{http://www.w3.org/2000/svg}text[@id="md2i_layer%i_title"]' % (self.layercounter))
       if title is not None:
         title.text = titletext
     self.layercounter += 1
     self.textcounter = 0
+    self.imagecounter = 0
 
-def handle_text(renderer, line):
-    text = inkex.etree.Element(inkex.addNS('text','svg'))
-    text.text = line
-    text.set('x', str(renderer.width / 10.))
-    text.set('y', str(renderer.textcounter * renderer.height / 15. + renderer.height/3.5))
-    #style = {'text-align' : 'center', 'text-anchor': 'middle'}
-    style = {'font-size': '28px',  'font-family': 'TeX Gyre Pagella'}
-    text.set('style', formatStyle(style))
-    renderer.layer.append(text)
-    renderer.textcounter += 1
+def handle_text(self, line):
+    already_existing_text = self.svg.find('.//{http://www.w3.org/2000/svg}text[@id="md2i_layer%i_text%i"]' % (self.layercounter, self.textcounter))
+    if already_existing_text is None:
+      text = inkex.etree.Element(inkex.addNS('text','svg'))
+      text.text = line
+      text.set('x', str(self.width / 10.))
+      text.set('y', str(self.textcounter * self.height / 15. + self.height/3.5))
+      text.set('id', "md2i_layer%i_text%i" % (self.layercounter, self.textcounter))
+      #style = {'text-align' : 'center', 'text-anchor': 'middle'}
+      style = {'font-size': '28px',  'font-family': 'TeX Gyre Pagella'}
+      text.set('style', formatStyle(style))
+      self.layer.append(text)
+    else:
+      already_existing_text.text = line
+    self.textcounter += 1
 
 def handle_image(self, src):
-    image = inkex.etree.Element(inkex.addNS('image','svg'))
-    image.set(inkex.addNS('href','xlink'), src)
-    image.set(inkex.addNS('absref','sodipodi'), os.environ['PWD'] + '/' + src) 
-    image.set('x', str(self.width / 10.))
-    image.set('y', str(self.height / 3.5))
-    style = {'image-align' : 'center', 'image-anchor': 'middle'}
-    image.set('style', formatStyle(style))
-    self.layer.append(image)
+    already_existing_image = self.svg.find('.//{http://www.w3.org/2000/svg}image[@id="md2i_layer%i_image%i"]' % (self.layercounter, self.imagecounter))
+    if already_existing_image is None:
+      image = inkex.etree.Element(inkex.addNS('image','svg'))
+      image.set(inkex.addNS('href','xlink'), src)
+      image.set(inkex.addNS('absref','sodipodi'), os.environ['PWD'] + '/' + src) 
+      image.set('x', str(self.width / 10.))
+      image.set('y', str(self.height / 3.5))
+      image.set('id', 'md2i_layer%i_image%i' % (self.layercounter, self.imagecounter))
+      style = {'image-align' : 'center', 'image-anchor': 'middle'}
+      image.set('style', formatStyle(style))
+      self.layer.append(image)
+    else:
+      already_existing_image.set(inkex.addNS('href','xlink'), src)
+      already_existing_image.set(inkex.addNS('absref','sodipodi'), os.environ['PWD'] + '/' + src) 
+    self.imagecounter +=1
 
 class InkslideRenderer(mistune.Renderer):
     """ modifies svg, dummy string output not needed"""
@@ -99,7 +112,7 @@ class InkslideRenderer(mistune.Renderer):
         handle_image(self, src)
         return "i"
     def autolink(self, link, is_email=False):
-        handle_text(self, text)
+        handle_text(self, link)
         return "a"
     def codespan(self, text):
         handle_text(self, text)
